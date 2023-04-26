19484a9ae8c6cffe9a26198fe47de94223604a90
Manlio Perillo <manlio.perillo@gmail.com>
2023-04-26 15:39:39 +0200

    DRAFT: run zig build in verbose mode
    This is a draft branch, used to help fixing an issue on windows-latest,
    where the Eowyn/build or CI/test freeze.
    
    The problem may be caused by a bug in Zig cache.

---
+++ ci/eowyn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Override the cache directories so that each `zig build` run will have a clean
+# cache.
+#
+# This fixes the freeze on windows-latest.
+export ZIG_GLOBAL_CACHE_DIR="$(pwd)/zig-global-cache"
+export ZIG_LOCAL_CACHE_DIR="$(pwd)/zig-local-cache"
+
+patches/eowyn.sh
